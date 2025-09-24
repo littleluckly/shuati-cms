001f5fd064813305473577c0f4fb835e06a40541
littleluckly <707798472@qq.com>
2025-09-24 19:15:49 +0800

    修改antd主题为绿色

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Layout, theme } from 'antd';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import SiderMenu from './components/SiderMenu';
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import QuestionList from './pages/QuestionList';
-import QuestionForm from './pages/QuestionForm';
-import NotFound from './pages/NotFound';
+import { Layout, theme, ConfigProvider } from "antd";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SiderMenu from "./components/SiderMenu";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import QuestionList from "./pages/QuestionList";
+import QuestionForm from "./pages/QuestionForm";
+import NotFound from "./pages/NotFound";
 
 const { Content } = Layout;
 
@@ -15,30 +15,43 @@ function App() {
   } = theme.useToken();
 
   return (
-    <Layout>
-      <SiderMenu />
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token，影响范围大
+          colorPrimary: "#00b96b",
+          borderRadius: 20,
+
+          // 派生变量，影响范围小
+          colorBgContainer: "#fff",
+        },
+      }}
+    >
       <Layout>
-        <Header />
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <Routes>
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/questions" element={<QuestionList />} />
-            <Route path="/questions/new" element={<QuestionForm />} />
-            <Route path="/questions/edit/:id" element={<QuestionForm />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Content>
+        <SiderMenu />
+        <Layout>
+          <Header />
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Routes>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/questions" element={<QuestionList />} />
+              <Route path="/questions/new" element={<QuestionForm />} />
+              <Route path="/questions/edit/:id" element={<QuestionForm />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 }
 
